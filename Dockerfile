fa3ea6266a9a3e7f0bd66e858ca6403828185d59
aloksport <137851522+aloksport@users.noreply.github.com>
2026-09-11 11:01:37 +0530

    Initial PHP application

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM php:8.5-apache
+
+COPY . /var/www/html/
+
+EXPOSE 80
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+phpinfo();
+echo "<h1>Hello from my PHP application!</h1>";
+echo "<p>Version 1.0</p>";
+echo "<p>Current date and time: " . date("Y-m-d H:i:s") . "</p>";
+echo "<p>PHP version: " . phpversion() . "</p>";
+echo "<p>Server software: " . $_SERVER['SERVER_SOFTWARE'] . "</p>";
+echo "<p>Client IP address: " . $_SERVER['REMOTE_ADDR'] . "</p>";
+echo "<p>Request method: " . $_SERVER['REQUEST_METHOD'] . "</p>";
+
+?>
